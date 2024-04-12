45cb300ad026e19916555898b9d8de59109fa7d5
Arnav Vats <arnav.vats@utexas.edu>
2024-04-11 20:50:00 -0500

    Created Navbar along with dark and light Theme

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+
+const NavBar = () => {
+  return (
+    <>
+      <HStack justifyContent={"space-between"} padding={"10px"}>
+        <HStack>
+          <Image src={logo} boxSize={"60px"} />
+          <Text>Game Nest</Text>
+        </HStack>
+        <ColorModeSwitch />
+      </HStack>
+    </>
+  );
+};
+
+export default NavBar;
